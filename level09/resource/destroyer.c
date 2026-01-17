1e5e483d454a3c7d45c1529ec217f4e6618ecca9
4trastos <93524280+4trastos@users.noreply.github.com>
2026-01-17 12:36:18 +0100

    update

--- level09/resource/destroyer.c
+++ level09/resource/destroyer.c
@@ -1,48 +1,7 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdio.h>
-#include <fcntl.h>
+import sys
 
-#define BUFFER_SIZE 1024
-
-int main (int argc, char **argv)
-{
-    char    buffer[BUFFER_SIZE];
-    char    c;
-    int     i;
-    int     fd;
-    ssize_t bytes;
-
-    if (argc != 2)
-    {
-        printf("Error: ( %s ): Necesita argumento\n", argv[0]);
-        return (1);
-    }
-
-    printf("Intentando abrir: %s\n", argv[1]);
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-        return (1);
-
-    bytes = read(fd, buffer, BUFFER_SIZE - 1);
-    if (bytes == -1)
-    {
-        printf("Error al leer el archivo\n");
-        close(fd);
-        return (1);
-    }
-
-    buffer[bytes] = '\0';
-
-    i = 0;
-    while (buffer[i] != '\0')
-    {
-        c = buffer[i] - i;
-        printf("%c", c);
-        i++;
-    }
-    close(fd);
-    printf("\n");
-    return (0);
-}
+hash = sys.argv[1]
+decrypted_hash = ""
+for i in range(0, len(hash)):
+    decrypted_hash = decrypted_hash + chr(ord(hash[i]) - i)
+print(decrypted_hash)
